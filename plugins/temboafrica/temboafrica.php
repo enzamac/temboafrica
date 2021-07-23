313e39ed235b32e30f95f7f9f1b3a8a2e601e5d8
Caesar Enzama <34058177+enzamac@users.noreply.github.com>
2021-07-23 16:36:23 +0300

    First

---
+++ plugins/temboafrica/temboafrica.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Tembo Africa Plugin
+ * Plgin URI: https://caesarenzama/temboafricaplugin
+ * Description: Example plugin
+ * Author: Caesar Enzama
+ * Author URI: https://caesarenzama.com
+ * Version: 0.1.1
+ */
